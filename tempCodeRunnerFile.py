966cdb42d3e87919afcc0156871bfcca9cbe2d4c
SAGAR T <ssagu6661@gmail.com>
2024-08-22 13:21:51 -0400

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+a=float(input("Enter the temperature in (cilsus)>>>     "))
+if a>35:
+    print("Cotton shirt and shorts")
+elif a>20:
+    print("Normal T-shirt and jeans ")
+elif a>15:
+    print("jarkeen and jeans pant and shoes")
+else :
+    print("wear the head cap,glouse and required items. or else you will suffer")    
